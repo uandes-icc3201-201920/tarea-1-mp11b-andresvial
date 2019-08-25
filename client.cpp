b1dcb395f8165b412805cf5c728d9c1f696cf7ca
mp11b <48453189+mp11b@users.noreply.github.com>
2019-08-25 19:31:26 -0400

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -9,10 +9,38 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+
+	string dir_socket;
+	int sflag = 0;
+	int opt;
+	
+ 	while ((opt = getopt (argc, argv, "s:")) != -1) 
+	{
+        	switch (opt)
+		{
+			/* Procesar el flag s si el usuario lo ingresa */
+			case 's':
+				sflag = 1;
+				dir_socket = optarg;
+				break;
+			default:
+				return EXIT_FAILURE;
+         	 }	    	
+   	 }
+	if(sflag == 0)
+	{
+		dir_socket == "/tmp/db.tuples.sock.";
+	}
 	
 	string cmd = "";
 	
 	while (cmd != "quit") {
+		if(cmd == "connect")
+		{
+			cout<<1<<endl;		
+		}
+		else if (cmd == "disconect")
+		{}
 		cout << ">";
 		cin >> cmd;
 	}
